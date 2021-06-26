42272cee0a9206e0404d070cf5fed803e17337c4
fluffycheeseball <billvolestate@gmail.com>
2021-06-26 13:43:50 +0100

    add partial deploy.sh

---
+++ .circleci/scripts/deploy.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -ex
+
+function usage() {
+    cat <<EOF 
+    #### deploy.sh ####
+    Deploy 
+      -r AWS region name
+      -e Deployment environment
+      Example:
+      deploy.sh -r eu-west-2 -e test
+EOF
+
+ xit 2
+}
+
+# set defaults
+aws_region = "eu-west-2"
+environment = "test"
+
+#read the input parametrs. OPTIND (option index) set to 1 so that all input parameters are read
+OPTIND=1
+
+while getopts "r:e:h" arg; do
+    case: $arg in
+    r)
+        aws_region=$OPTARG
+        ;;
+    e)
+        environment=$OPTARG
+        ;;
+    *)
+        usage
+        ;;
+    esac
+done
+shift "((OPTIND-1))
+
+# Load global variables
