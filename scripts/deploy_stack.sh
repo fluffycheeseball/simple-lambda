2f190d3ebbb35e3035e7226d9f26914a02841033
fluffycheeseball <billvolestate@gmail.com>
2021-06-27 17:23:45 +0100

    fix variable

--- scripts/deploy_stack.sh
+++ scripts/deploy_stack.sh
@@ -21,7 +21,6 @@ exit 2
 target_stack_name=""
 template_location=""
 parameter_file_location=""
-use_git_commit='false'
 
 #read the input parametrs. OPTIND (option index) set to 1 so that all input parameters are read
 OPTIND=1
@@ -46,9 +45,10 @@ shift "$((OPTIND-1))"
 echo "Deploying stack: ${target_stack_name}"
 echo "Using template: ${template_location}"
 echo "Using parameters: ${parameter_file_location}"
-echo "Using git commits: ${use_git_commit}"
 
 internal_param_file_location=parameters.json
+
+cat ${parameter_file_location}
 
 cp ${parameter_file_location} parameters.json.tmp 
 jq --arg gitsha ${CIRCLE_SHA1} --arg buildNumber ${CIRCLE_BUILD_NUM} '. + [ { "ParameterKey":"GitCommit", "ParameterValue":$gitsha }, {"ParameterKey":"CircleCIBuildNumber", "ParameterValue":$buildNumber} ] ' <parameters.json.tmp > $internal_param_file_location
