6d759035266640eaf43aea26ea0385d6528bd27c
fluffycheeseball <billvolestate@gmail.com>
2021-07-10 17:40:58 +0100

    test

--- scripts/deploy_stack.sh
+++ scripts/deploy_stack.sh
@@ -90,7 +90,7 @@ else
   cat ~/.aws/config
 
   aws ${aws_env_str} cloudformation create-stack \
-    --stack-name "${target_stack_name}" \
+    --stack-name ${target_stack_name} \
     --template-body "file://$template_location" \
     --parameters "file://$internal_param_file_location" \
     --capabilities CAPABILITY_NAMED_IAM \
