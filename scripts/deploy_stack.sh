a6ede7c0e8b4d135b66aef3f363cec76173e14e2
fluffycheeseball <billvolestate@gmail.com>
2021-07-25 20:19:31 +0100

    temp

--- scripts/deploy_stack.sh
+++ scripts/deploy_stack.sh
@@ -80,7 +80,7 @@ if aws --profile=$DEPLOYMENT_PROFILE_TEST cloudformation describe-stacks --stack
 
 else
   echo "creating...2"
-  aws --profile=$DEPLOYMENT_PROFILE_TEST cloudformation create-stack  --stack-name "jude-simple-lambda-infrastructure-stack-test-env"  --template-body "file://$template_location" \
+  aws --profile=$DEPLOYMENT_PROFILE_TEST cloudformation create-stack  --stack-name ${target_stack_name}  --template-body "file://$template_location" \
     --parameters "file://$internal_param_file_location" \
     --capabilities CAPABILITY_NAMED_IAM \
     --role-arn "${cloudformation_role}"
